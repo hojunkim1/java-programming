5cee8d59dfc715646f28bb0db4394fb3a65755f6
Hojun Kim <hojunkim02@icloud.com>
2023-04-05 11:08:46 +0900

    Create Writer.java

---
+++ src/Writer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Writer {
+    public static void main(String[] args) throws IOException {
+        File file = new File("test.txt");
+
+        // 파일 생성
+        if (file.createNewFile())
+            System.out.println("File created: " + file.getName());
+        else
+            System.out.println("File already exists.");
+
+        // 파일 쓰기
+        FileWriter writer = new FileWriter(file);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        writer.write(br.readLine());
+        writer.close();
+        br.close();
+
+        // 파일 읽기
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            System.out.println(data);
+        }
+        reader.close();
+    }
+}
